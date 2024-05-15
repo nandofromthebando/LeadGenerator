24f693ea6e25e3c4cf403775999184c8d75f1c50
Anand Lessard <87321214+nandofromthebando@users.noreply.github.com>
2024-05-15 17:47:54 -0400

    Update BackendTestFile.py
    Some testing to start with backend mechanics.

--- BackendTestFile.py
+++ BackendTestFile.py
@@ -3,6 +3,13 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import time
 
-driver = webdriver.Chrome('/Users/nandolessard/Desktop/Google Chrome.app')
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=options)
 
-driver.get("https://www.linkedin.com")
+driver.get("https://www.linkedin.com/search/results/people/?keywords=real%20estate%20agents&origin=SWITCH_SEARCH_VERTICAL&searchId=eb6c74ed-da5a-4aec-b51e-aa4ab7f5459e&sid=9cj")
+
+soup = BeautifulSoup(driver.page_source, 'lxml')
+
+boxes = soup.find_all('li',class_ ='reusable-search__result-container')
+print(len(boxes))
