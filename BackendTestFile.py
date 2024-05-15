d4b59180d921b560a51c0e0bfe9a7dc242a43e65
Anand Lessard <nando.lessard@gmail.com>
2024-05-15 16:41:52 -0400

    Update BackendTestFile.py

--- BackendTestFile.py
+++ BackendTestFile.py
@@ -3,4 +3,6 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import time
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome('/Users/nandolessard/Desktop/Google Chrome.app')
+
+driver.get("https://www.linkedin.com")
