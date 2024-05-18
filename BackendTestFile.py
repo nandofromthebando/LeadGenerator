cfa63636007983c8fb4a08a8315e961739a29665
Anand Lessard <87321214+nandofromthebando@users.noreply.github.com>
2024-05-17 21:25:46 -0400

    Update BackendTestFile.py

--- BackendTestFile.py
+++ BackendTestFile.py
@@ -29,11 +29,28 @@ print(len(boxes))
 
 df = pd.DataFrame({'Link':[''], 'Name':[''], 'Job Title':[''], 'Location':['']})  # Create a df to store all the information
 
-# Extract information from each box (example)
-for i in boxes:
-    link = i.find('a').get('href')
-    # Assuming there's a name field in the HTML structure
-    name = i.find('span', {'dir','ltr'}).find('span',{'app-aware-link':'true'})  
-    title = i.find('div',class_ = 'entity-result__primary-subtitle t-14 t-black t-normal').text
-    location = i.find('div',class_ = 'entity-result__secondary-subtitle t-14 t-normal').text
-    df.append(({'Link':link, 'Name':name, 'Job Title':title, 'Location':location}))
+while True:
+    # Parse the page source with BeautifulSoup
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+
+    # Find all elements with the specified class
+    boxes = soup.find_all('li', class_='reusable-search__result-container')  # Boxes HTML variables, contain profiles
+    # Extract information from each box (example)
+    for i in boxes:
+        try:
+            link = i.find('a').get('href')
+            # Assuming there's a name field in the HTML structure
+            name = i.find('span', {'dir','ltr'}).find('span',{'app-aware-link':'true'})  
+            title = i.find('div',class_ = 'entity-result__primary-subtitle t-14 t-black t-normal').text
+            location = i.find('div',class_ = 'entity-result__secondary-subtitle t-14 t-normal').text
+            df.append(({'Link':link, 'Name':name, 'Job Title':title, 'Location':location}))
+        except:
+            pass
+    # For scrolling to the bottom of the page
+    driver.execute_script('window.scrollTo(0,document.body.scroll Hieght)')
+    time.sleep(1)
+
+    # For clicking through the pages pg 2 and so on (skip page 1 different html)
+    driver.find_elemnt_by_xpath('/html/body/div[5]/div[3]/div[2]/div/div[1]/main/div/div/div[4]/div/div/button[2]').click()
+    time.sleep(3)
+
