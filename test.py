6bddac90ae6de334c053c9adaf2e279e4e92e6d4
Anand Lessard <nando.lessard@gmail.com>
2024-05-18 14:22:50 -0400

    Update test.py

--- test.py
+++ test.py
@@ -23,19 +23,19 @@ def custom_google_search(query, language="en", region="US"):
 
     # Get the page source after some time for dynamic content to load
     driver.implicitly_wait(10)  # Wait for up to 10 seconds for elements to load
-    page_source = driver.page_source
-    print(page_source)
-    # Parse the HTML source using Beautiful Soup
-    soup = BeautifulSoup(page_source, "html.parser")
-    print("passed")
-    # Find all the search result titles using the appropriate CSS selector
-    search_results = soup.select("a.hfpxzc")
-    print("passed")
-    # Print the titles of the search results
-    for result in search_results:
-        print(result.get_text())
+    # Parse the page source with BeautifulSoup
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+
+    # Find all elements with the specified class
+    boxes = soup.find_all('li', class_='hfpxzc')  # Boxes HTML variables, contain profiles of businesses
+
+    # Test if they are there
+    print(len(boxes))
+
 '''    while(True):
         pass'''
-# Example usage
-query = "medical salons near 02301"
+
+
+# Test usage before adding it to the main
+query = "medical salons near 02301" # Google Search Result
 custom_google_search(query)
